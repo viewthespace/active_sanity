a68ac5652bdd2e536fecfa1388ec7033da913309
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2011-04-01 17:49:38 -0700

    Fixing an issue which caused cucumber to fail at the beginning itself if the project is forked for the first time into a new location and the cucumber tests are tried to be run

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,13 +4,15 @@ require 'rubygems'
 require "bundler"
 Bundler.setup
 
-system("rm ./test/rails_app/db/migrate/*create_invalid_records.rb")
-
-raise unless File.directory?("test/rails_app") && system("cd test/rails_app && rake db:drop db:create db:migrate")
+if File.directory?("test/rails_app")
+  Dir.chdir("test/rails_app") do
+    raise unless system("rm -f db/migrate/*create_invalid_records.rb && rake db:migrate:reset")
+  end
+end
 
 After do
   # Reset DB!
-  User.delete_all
+  User.delete_all if 
   Category.delete_all
   Post.delete_all
   InvalidRecord.delete_all if InvalidRecord.table_exists?
